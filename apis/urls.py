931042eb70e67ed2478b0f9f786baedfecc08e1a
Kabhilan05 <kabhilan05@gmail.com>
2023-12-05 08:57:49 +0530

    Login,logout,register

--- apis/urls.py
+++ apis/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register, getUser
+from .views import RegistrationAPIView, LoginAPIView, LogoutAPIView
 
 urlpatterns = [
-    path("register/",register, name="register"),
-    path("getUsers/",getUser, name="getUsers")
+    path('register/', RegistrationAPIView.as_view(), name='register'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
 ]
